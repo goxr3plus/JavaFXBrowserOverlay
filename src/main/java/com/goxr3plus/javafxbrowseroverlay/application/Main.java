fae1660236fc16fc8a9f3bd69a0a681a67ceefc1
GOXR3PLUS STUDIO <crAlexcomp@gmail.com>
2017-12-08 20:47:37 +0200

    Updated

--- src/main/java/com/goxr3plus/javafxbrowseroverlay/application/Main.java
+++ src/main/java/com/goxr3plus/javafxbrowseroverlay/application/Main.java
@@ -1,15 +1,31 @@
 package main.java.com.goxr3plus.javafxbrowseroverlay.application;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import main.java.com.goxr3plus.javafxwebbrowser.browser.WebBrowserController;
 
-public class Main extends Application{
-
+public class Main extends Application {
+	
+	WebBrowserController webBrowser = new WebBrowserController();
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		
+		BorderPane borderPane = new BorderPane(webBrowser);
 		
+		Scene scene = new Scene(borderPane);
 		
+		//Create the PrimaryStage
+		primaryStage.setTitle("Scene");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
 	}
 	
 }
