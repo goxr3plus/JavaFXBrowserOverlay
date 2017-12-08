1265fcedf76600bbeb77a32ed0753f9381b67d35
GOXR3PLUS STUDIO <crAlexcomp@gmail.com>
2017-12-08 20:30:09 +0200

    Added Files

---
+++ src/main/java/com/goxr3plus/javafxbrowseroverlay/application/Main.java
@@ -0,0 +1,15 @@
+package main.java.com.goxr3plus.javafxbrowseroverlay.application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		
+		
+	}
+	
+}
